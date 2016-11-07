66975660c27561760a99f28532220030450ef6bf
iUndeined <869535088@qq.com>
2016-11-07 11:03:17 +0800

    nothing
    一点小细节

--- src/main/java/me/cjd/hibernate/factory/SpringSessionDAO.java
+++ src/main/java/me/cjd/hibernate/factory/SpringSessionDAO.java
@@ -1,13 +1,11 @@
 package me.cjd.hibernate.factory;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import me.cjd.hibernate.impl.QueryDAOImpl;
 
 public class SpringSessionDAO<T, ID extends java.io.Serializable> extends QueryDAOImpl<T, ID> {
 	
 	private Session session;
-	private SessionFactory sessionFactory;
 	
 	public Session getSession() {
 		return session;
@@ -15,14 +13,6 @@ public class SpringSessionDAO<T, ID extends java.io.Serializable> extends QueryD
 	
 	public void setSession(Session session) {
 		this.session = session;
-	}
-	
-	public SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
-	
-	public void setSessionFactory(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
 	}
 	
 }
